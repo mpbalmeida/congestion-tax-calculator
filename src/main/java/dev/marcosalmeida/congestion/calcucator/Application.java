e8c956d79aebdce00a4e9ee4e9de48f707c47f3f
marcos.almeida <me@marcosalmeida.dev>
2022-10-23 18:33:03 +0200

    chore: add spring boot web application

---
+++ src/main/java/dev/marcosalmeida/congestion/calcucator/Application.java
@@ -0,0 +1,13 @@
+package dev.marcosalmeida.congestion.calcucator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
--- src/main/java/dev/marcosalmeida/congestion/calcucator/Car.java
+++ src/main/java/dev/marcosalmeida/congestion/calcucator/Car.java
@@ -1,7 +1,4 @@
-package congestion.calculator;
-
-import java.util.*;
-import java.text.*;
+package dev.marcosalmeida.congestion.calcucator;
 
 public class Car implements Vehicle {
 
